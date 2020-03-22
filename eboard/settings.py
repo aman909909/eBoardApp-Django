e8a9d290bfcdf428624428fcf527afe118e74671
aman909909 <amanashish909@gmail.com>
2020-03-23 00:38:54 +0530

    Change settings

--- eboard/settings.py
+++ eboard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+bxppj=d=7t!vzkj1l+-(qohn85-0@w1(f)0v-@q7f7*+bb0e1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
