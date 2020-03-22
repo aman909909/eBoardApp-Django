5dd3df6f93f4bd429eeb0609488445b99d557301
aman909909 <amanashish909@gmail.com>
2020-03-23 02:21:20 +0530

    Change settings

--- eboard/settings.py
+++ eboard/settings.py
@@ -82,8 +82,12 @@ TEMPLATE_DIRS = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'eboardapp.herokuapp.com',
+        'PORT': '',
     }
 }
 
