2f8367c4df41151437b2fde200587faf3d24eec1
aman909909 <amanashish909@gmail.com>
2020-03-25 01:17:11 +0530

    few cahnges

--- eboard/settings.py
+++ eboard/settings.py
@@ -82,8 +82,12 @@ TEMPLATE_DIRS = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
