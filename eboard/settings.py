06008a1b01a6ee15ca7879574aec6a2b04437453
aman909909 <amanashish909@gmail.com>
2020-03-29 01:05:27 +0530

    Deploy

--- eboard/settings.py
+++ eboard/settings.py
@@ -17,9 +17,9 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
-#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -84,22 +84,22 @@ TEMPLATE_DIRS = (
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   ##    'USER': 'amanashish',
-     #   'PASSWORD': 'm191007005',
-      #  'HOST': '125.125.125.125',
-       # 'PORT': '5432',
-    #}
-#}
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'USER': 'amanashish',
+        'PASSWORD': 'm191007005',
+        'HOST': '125.125.125.125',
+        'PORT': '5432',
     }
 }
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
 
 
 # Password validation
