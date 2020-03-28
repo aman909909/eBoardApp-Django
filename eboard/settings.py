e8f2368c18998c05cd4773312ab8440759a685be
aman909909 <amanashish909@gmail.com>
2020-03-29 01:08:04 +0530

    Deploy

--- eboard/settings.py
+++ eboard/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 #DATABASES = {
  #   'default': {
   #      'ENGINE': 'django.db.backends.sqlite3',
